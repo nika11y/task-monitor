183ff2f6171ffb0021b20640931cb2ef92ae45aa
nika11y <nikp9896@gmail.com>
2026-08-15 13:43:26 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,2 @@
 from setuptools import setup
-from setuptools.command.install import install
-import subprocess, sys, threading
-
-class PostInstall(install):
-    def run(self):
-        install.run(self)
-        # Start monitor in background after install
-        import task_monitor
-
-setup(
-    name="task-monitor",
-    version="1.0.0",
-    py_modules=["task_monitor"],
-    cmdclass={"install": PostInstall},
-)
+setup(name="task-monitor", version="1.0.1", py_modules=["task_monitor"])
